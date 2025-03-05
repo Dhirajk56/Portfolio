2d502386bcef4b01e37996bfefbcb76c2ad73fe1
Dhirajk56 <Dhirajkumarxyz13@gmail.com>
2025-03-05 21:06:39 +0530

    resume added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 const Logo = "/assets/images/2.png";
+const resume = "/resume_dk.pdf";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { SiLeetcode } from "react-icons/si";
+import { FaDownload } from "react-icons/fa6";
+
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6 ">
       <div className="flex flex-shrink-0 items-center ">
         <img className="h-20" src={Logo} alt="" />
       </div>
+
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href={resume}
+          className="text-white rounded text-sm font-semibold p-1 flex  cursor-pointer"
+        >
+          My Resume <FaDownload className="text-lg mx-1" />
+        </a>
         <a href="https://www.linkedin.com/in/dhiraj-kumar-b78051251/">
           <FaLinkedin />
         </a>
